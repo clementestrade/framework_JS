aa5416842f77654ee8d026a488752a012b140622
Tom <trichard.dev@gmail.com>
2019-03-24 15:11:16 +0100

    modif map bug

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 // ============== LEAFLET ==============
 
-var latlong = null;
+var latlong = L.latLng(45.833619,1.261105);
 var mymap = null;
 var testMap = $('#testMap');
 var mapErrors = $('#mapErrors');
@@ -64,9 +64,7 @@ var ruchers = [];
 
     getData();
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(constructMap, showError);
-    }
+
 
     const app = new Vue({
         el: '#app',
@@ -75,17 +73,23 @@ var ruchers = [];
         },
         watch: {
             ruchers: function (oldRuchers, newRuchers) {
-                console.log(oldRuchers);
-                console.log(newRuchers);
                 if(newRuchers.length > 0) {
                     newRuchers.forEach(function(rucher, index){
+                        mymap = L.map('testMap').setView(latlong, 13);
+                        var container = L.DomUtil.get('testMap');
+                        if(container != null){
+                            container._leaflet_id = null;
+                            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidHJpY2hhcmQiLCJhIjoiY2p0ZDNnMjFuMGdkejN6cGJwcndsOXI5MyJ9.hSogqqC1DJ9KRsFTCvYfTA', {
+                                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                                maxZoom: 18,
+                                id: 'mapbox.streets',
+                                accessToken: 'your.mapbox.access.token'
+                            }).addTo(mymap);
+                        }
                         if( rucher.latitude !== undefined && rucher.longitude !== undefined ){
                             var templatlong = L.latLng(rucher.latitude, rucher.longitude);
                             var marker = L.marker(templatlong).addTo(mymap);
                             marker.bindPopup('<h5><b>' + rucher.nom + '</b></h5><p>Nombre de ruches : ' + rucher.nbRuches + '</p><a href="/visiteRuche.html?id=' + rucher.identifiant +'" class="btn btn-warning text-light">Visitez le rucher</a>');
-                            if(index === 0){
-                                marker.openPopup();
-                            };
                         }
                     });
                 }
@@ -97,13 +101,6 @@ var ruchers = [];
 
 function constructMap(position) {
     latlong = L.latLng(position.coords.latitude,position.coords.longitude);
-    mymap = L.map('testMap').setView(latlong, 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidHJpY2hhcmQiLCJhIjoiY2p0ZDNnMjFuMGdkejN6cGJwcndsOXI5MyJ9.hSogqqC1DJ9KRsFTCvYfTA', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'your.mapbox.access.token'
-    }).addTo(mymap);
 }
 
 function showError(error) {
